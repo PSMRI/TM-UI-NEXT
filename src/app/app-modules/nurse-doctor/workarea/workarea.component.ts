c1c6cd1f21ae919510034670cb51c579d05687bb
Pratibha <bhallamudi.pratibha@wipro.com>
2024-05-29 15:31:57 +0530

    added fix for the on change of visit category refresh the history pages

--- src/app/app-modules/nurse-doctor/workarea/workarea.component.ts
+++ src/app/app-modules/nurse-doctor/workarea/workarea.component.ts
@@ -835,7 +835,7 @@ export class WorkareaComponent
     this.patientMedicalForm.removeControl('patientANCForm');
     this.patientMedicalForm.removeControl('patientCaseRecordForm');
     this.patientMedicalForm.removeControl('patientReferForm');
-    // this.patientMedicalForm.removeControl('NCDScreeningForm');
+    this.patientMedicalForm.removeControl('NCDScreeningForm');
     this.patientMedicalForm.removeControl('idrsScreeningForm');
 
     this.showQuickConsult = false;
@@ -847,6 +847,7 @@ export class WorkareaComponent
     this.showPNC = false;
     this.showCaseRecord = false;
     this.showRefer = false;
+    this.changeDetectorRef.detectChanges();
   }
 
   submitPatientMedicalDetailsForm(medicalForm: any) {
